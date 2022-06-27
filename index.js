970d8bd78f5764db4941c6f8cffafcf91737832f
final3blindside <final3blindside@gmail.com>
2022-06-27 23:33:11 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/*function greetings() {
+    var name = prompt("Welcome to the Lost Treasures Index!  May I have your name?");
+        if (name === "JanRex", "Isaac", "Roimarc", "Yancey", "Miguel") {
+            alert("You are a Rastaman! Welcome and Go see all the treasures of our world!")
+        } else if(name === "Yunna", "Michelle", "Yancey", "Lemuel") {
+            alert('You are from the Labay Household!')
+        } else {
+            alert("Welcome Visitor! Ready to see the challenges surrounding the treasures of our world?")
+        }
+}
+
+greetings();
+
+
+alert(name);*/
+
+
+function Addition() {
+    num1 = 4
+    num2 = 7
+
+    total = num1 + num2;
+}
+
+Addition(total);
